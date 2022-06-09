244e087228ba831601c00b7f8d322b78a1209fa3
Ednario Andrade <joseednario637@gmail.com>
2022-06-09 12:15:28 -0300

    debug-application

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,16 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+
+app.post("/courses", (req, res) => {
+  const { name } = req.body;
+  return res.json( { name } );
 });
 
 app.listen(3333, () => {

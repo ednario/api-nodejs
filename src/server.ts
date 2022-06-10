6bee1f74b5f546effbf8542ceadb1cde3d79fd82
Ednario Andrade <joseednario637@gmail.com>
2022-06-10 01:59:29 -0300

    UUID insertion and project pattern fixes.

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,4 @@ app.use(express.json());
 
 app.use(categoriesRoutes);
 
-app.listen(3333, () => {
-  console.log("Server is running on port 3333");
-});
+app.listen(3333, () => console.log("Server is running"));

4f25f229c4d4c71df6395fbe7a16ea28055009b6
Ednario Andrade <joseednario637@gmail.com>
2022-06-10 01:25:03 -0300

    created 'categories' route.

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,11 @@
 import express from "express";
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-
-app.post("/courses", (req, res) => {
-  const { name } = req.body;
-  return res.json( { name } );
-});
+app.use(categoriesRoutes);
 
 app.listen(3333, () => {
   console.log("Server is running on port 3333");
